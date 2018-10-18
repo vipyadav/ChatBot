1444ac1919af4964b5c70bdd4b585cf1120fcff6
Vipin Kumar Yadav <er.vipinyadav@gmail.com>
2018-10-18 22:35:13 +0530

    GreetingDialog is added and launch on startup instead of RootDialog.

---
+++ MrVIP/MrVIP/Dialogs/GreetingDialog.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace MrVIP.Dialogs
+{
+    [Serializable]
+    public class GreetingDialog : IDialog
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.PostAsync("Hi I'm Mr.VIP.");
+
+            context.Wait(MessageReceivedAsync);
+            return Task.CompletedTask;
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            var userName = String.Empty;
+            var getName = false;
+            context.UserData.TryGetValue<string>("Name", out userName);
+            context.UserData.TryGetValue<bool>("GetName", out getName);
+
+            if (getName)
+            {
+                userName = message.Text;
+                context.UserData.SetValue<string>("Name", userName);
+                context.UserData.SetValue<bool>("GetName", false);
+                
+            }
+            if (string.IsNullOrEmpty(userName))
+            {
+                await context.PostAsync("What is your name?");
+                context.UserData.SetValue<bool>("GetName", true);
+            }
+            else
+                await context.PostAsync($"Hi {userName}, How can I help you?");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
